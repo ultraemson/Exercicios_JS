a1672975487f684357c75e8476b5c50b0727d41b
Emson França <esf12364@gmail.com>
2023-03-19 22:47:11 -0300

    new commit

---
+++ questao/018/index.js
@@ -0,0 +1,9 @@
+// export function questao(lines){
+//     const quantidade = lines[0].split(' ')
+// }
+//const quantidade = parseInt(prompt('Valor de entrada'))
+const vetor = [n100 = 100,n50=50,n20=20,n10=10,n5=5,n2=2,n1=1]
+let element =0
+for (let i = 0; i < vetor.length; i++) {
+    element = vetor[i]%0;
+}
